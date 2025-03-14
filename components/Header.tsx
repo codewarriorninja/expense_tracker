db4d8eecd87488c33108a71f32da76297e495d56
Henok <henokaragaw17@gmail.com>
2025-03-14 14:57:47 -0900

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import { SignedIn,SignedOut,UserButton } from "@clerk/nextjs"
+import Link from "next/link"
+import { checkUser } from "@/lib/checkUser"
+
+const Header = async() => {
+ const user = await checkUser();
+ console.log(user);
+ 
+  return (
+    <nav className ='navbar'>
+      <div className="navbar-container">
+        <h2>Expense Tracker</h2>
+        <div>
+          <SignedOut>
+            <Link href={'sign-in'}>
+             <button className="cursor-pointer">Sign in</button>
+            </Link>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
