a9cf0248526ae0995c84fd4b43b9cae56e414da4
Henok <henokaragaw17@gmail.com>
2025-03-15 12:23:21 -0900

    update transaction data

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Guest from "@/components/Guest"
 import { currentUser } from "@clerk/nextjs/server"
+import AddTransaction from "@/components/AddTransaction";
+import Balance from "@/components/Balance";
 
 const page = async() => {
   const user = await currentUser();
@@ -11,6 +13,8 @@ const page = async() => {
   return (
     <main>
       <h1>Welcome, {user.firstName}</h1>
+      <Balance />
+      <AddTransaction />
     </main>
   )
 }
